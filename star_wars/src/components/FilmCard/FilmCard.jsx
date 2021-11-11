3ff17e8a8a601cac585aacb17d63a885e803be3d
zehavit-levi <levi.zehavit@gmail.com>
2021-11-11 14:19:24 +0200

    add logic add/remove from favorites + icons

--- star_wars/src/components/FilmCard/FilmCard.jsx
+++ star_wars/src/components/FilmCard/FilmCard.jsx
@@ -1,14 +1,33 @@
 import './FilmCard.css';
 import React  from 'react';
+import { BsSuitHeartFill,  BsSuitHeart} from 'react-icons/bs';
 
-export default function FilmCard({film}){
+export default function FilmCard({film,favorites,setFavorites}){
+
+   const updateFav = (filmTilte,action)=> {
+      let favoriteArr;
+      if(action === "add"){
+         favoriteArr = favorites;
+         favoriteArr.push(filmTilte);
+      } else {
+         favoriteArr = favorites.filter((favorite)=>favorite!==filmTilte)
+      }
+      setFavorites([...favoriteArr]);
+      localStorage.setItem("favorites",JSON.stringify(favorites));
+   }
  return(
     <div className="film_container">
        <div>{film["title"]}</div>
        <div>
           <div className="title">{film["title"]}</div>
           <div className="abstract">{film["opening_crawl"]}</div>
-          <button type="button" >add to favorite</button>
+          {
+             favorites.includes(film["title"]) ? 
+             <BsSuitHeartFill onClick={()=>updateFav(film["title"])} style={{color:'red'}}/>
+             :
+            <BsSuitHeart onClick={()=>updateFav(film["title"],"add")} style={{color:'red'}}/> 
+             
+          }
        </div>
     </div>
  )
