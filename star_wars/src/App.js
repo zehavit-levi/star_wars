e81d302b8966601fbac290f163a1f13727c4719e
zehavit-levi <levi.zehavit@gmail.com>
2021-11-11 12:56:49 +0200

    add get films from api and render results

--- star_wars/src/App.js
+++ star_wars/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
-import React from 'react';
+import React , { useState ,useEffect }from 'react';
+import axios from "axios";
+import FilmsList from './components/FilmsList/FilmsList';
+
 
 function App() {
+  const [results, setResults] = useState(null);
+  const [favoritsList, setFavoritsList] = useState([]);
+
+  const searchURL="https://swapi.dev/api/films/";
+    axios.get(searchURL).then(response => {
+      setResults(response.data.results)
+    });
+  
+
   return (
     <div className="App">
-    
+      {results!=null ?
+     <FilmsList filmsList={results} favoritsList={favoritsList}/>:
+     <></>}
     </div>
   );
 }
