8748f5a093475cd3f25f5d2591434bd842cd9792
zehavit-levi <levi.zehavit@gmail.com>
2021-11-14 12:57:04 +0200

    add more design

--- star_wars/src/components/FilmCard/FilmCard.jsx
+++ star_wars/src/components/FilmCard/FilmCard.jsx
@@ -3,34 +3,29 @@ import React  from 'react';
 import { BsSuitHeartFill,  BsSuitHeart} from 'react-icons/bs';
 
 export default function FilmCard({film,favorites,setFavorites}){
-
-   const updateFav = (filmTilte,action)=> {
+   const updateFav = (filmTilte)=> {
       let favoriteArr;
-      if(action === "add"){
+      if(!favorites.includes(filmTilte)){
          favoriteArr = favorites;
          favoriteArr.push(filmTilte);
       } else {
-         favoriteArr = favorites.filter((favorite)=>favorite!==filmTilte)
-         console.log(favoriteArr)
+         favoriteArr = favorites.filter((favorite)=>favorite!==filmTilte);
       }
       setFavorites([...favoriteArr]);
       localStorage.setItem("favorites",JSON.stringify([...favoriteArr]));
 
    }
  return(
-    <div className="film_container">
-       <div className="title">{film["title"]}</div>
-       <div className="details">
-          <div className="title">{film["title"]}</div>
-          <div className="abstract">{film["opening_crawl"]}</div>
+      <div className="details">
+         <div className="title">{film["title"]}</div>
+         <div className="abstract">{film["opening_crawl"]}</div>
           {
              favorites.includes(film["title"]) ? 
-             <BsSuitHeartFill onClick={()=>updateFav(film["title"])} style={{color:'red'}}/>
+             <BsSuitHeartFill className="hearticon"  onClick={()=>updateFav(film["title"])} style={{color:'red'}}/>
              :
-            <BsSuitHeart onClick={()=>updateFav(film["title"],"add")} style={{color:'red'}}/> 
+            <BsSuitHeart className="hearticon" onClick={()=>updateFav(film["title"])} style={{color:'red'}}/> 
              
           }
-       </div>
-    </div>
- )
+      </div>
+   )
 }
